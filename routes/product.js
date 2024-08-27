ff4ce36687a8a546b08cf15c1659569e3261a4b8
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2024-08-27 19:00:50 +0900

    Feat: 상품 업데이트 기능 #4

--- routes/product.js
+++ routes/product.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const productController = require("../controllers/productController");
-// require("dotenv").config();
+const {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+} = require("../controllers/productController");
 
 // express.Router()에 이 데이터를 저장하는 행위구만, 최종적으로 Router를 export해서 server.js에서 사용하는거지
 // 모든 상품 가져오기
-router.get("/", productController.getProducts);
+router.get("/", getProducts);
 
 // 특정 상품 가져오기
-router.get("/:id", productController.getProductById);
+router.get("/:id", getProductById);
 
 // 새로운 상품 생성
-router.post("/create", productController.createProduct);
+router.post("/create", createProduct);
+
+// 특정 상품 업데이트
+router.post("/update/:id", updateProduct);
 
 module.exports = router;
